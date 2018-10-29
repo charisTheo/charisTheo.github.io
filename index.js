a4be4d69d2223b2fdb344c47aef2732be46783bf
charisTheo <haristh@hotmail.com>
2018-10-29 16:27:24 +0100

    Add perspective hover effect on cards

--- index.js
+++
@@ -1,9 +0,0 @@
-particlesJS.load('particles-js', './particles/particles-config.json');
-const cardToggles = document.getElementsByClassName('card-toggle');
-
-for (let cardToggle of cardToggles) {
-    cardToggle.addEventListener("click", function(event) {
-        event.preventDefault();
-        this.parentElement.classList.toggle('toggle');
-    });
-}
---
+++ js/index.js
@@ -0,0 +1,9 @@
+particlesJS.load('particles-js', './particles/particles-config.json');
+// const cardToggles = document.getElementsByClassName('card-toggle');
+
+// for (let cardToggle of cardToggles) {
+//     cardToggle.addEventListener("click", function(event) {
+//         event.preventDefault();
+//         this.parentElement.classList.toggle('toggle');
+//     });
+// }

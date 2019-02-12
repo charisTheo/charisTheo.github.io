cacda78fdc3580b7de4b19536d35cca551775348
charisTheo <haristh@hotmail.com>
2019-02-12 15:00:24 +0000

    add image picker directive

--- js/app.js
+++ js/app.js
@@ -28,4 +28,19 @@ app
         restrict: 'E',
         templateUrl: '/partials/page-loader.html'
     }
+})
+.directive('imagePicker', function() {
+    return {
+        scope: false,
+        restrict: 'E',
+        templateUrl: '/partials/imagePicker.html',
+        link: function(scope, elm, attrs) {
+            scope.images = JSON.parse(attrs.images);
+            scope.selectedImage = 0;
+            scope.selectImage = function(index) {
+                scope.selectedImage = index;
+                console.log(index);
+            }
+        }
+    }
 });

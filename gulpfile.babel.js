060bd6e345d17b2d8ad3e732b695bfa14c00817e
charisTheo <haristh@hotmail.com>
2019-08-02 13:00:42 +0100

    Added media query for dark theme preference. Fixed gulp tasks.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,11 +6,17 @@ let concat = require('gulp-concat');
 let ngAnnotate = require('gulp-ng-annotate');
 // let ngAnnotate = require('babel-plugin-angularjs-annotate');
 let uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 let browserSync = require('browser-sync');
 let server = browserSync.create();
 
 function reload(done) {
     server.reload();
+    return done();
+}
+
+function stream(done) {
+    server.stream();
     return done();
 }
   
@@ -25,47 +31,38 @@ function serve(done) {
 
 const clean = () => del(['styles/css/compressed/*', 'js/bundle/*']);
 
-gulp.task('bundle-js', function(done) {
+gulp.task('minify-js', function(done) {
     gulp.src([
         'js/app.js',
         'js/components/portfolio.ctr.js',
-        'js/components/share-listener.fac.js',
-        'js/accelerometer.js',
+        'js/components/share-listener.fac.js'
     ])
-    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.min.js'))
     .pipe(ngAnnotate())
     .pipe(babel())
-    .pipe(gulp.dest('js/bundle/'));
-    
-    return done();
-});
-gulp.task('minify-js', function(done) {
-    gulp.src('js/bundle/bundle.js')
-    .pipe(concat('bundle.min.js'))
-    .pipe(gulp.dest('js/bundle/'))
     .pipe(uglify())
-    .on('error', (error) => console.log(error))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('js/bundle/'));
-    
+
     return done();
 });
 
 gulp.task('minify-css', function(done) {
     gulp.src('styles/css/*.css')
-        .pipe(minifyCSS())
-        .pipe(concat('styles.min.css'))
-        .pipe(gulp.dest('styles/css/compressed'));
+    .pipe(minifyCSS())
+    .pipe(concat('styles.min.css'))
+    .pipe(gulp.dest('styles/css/compressed'));
+
     return done();
 });
 
-const watch = () => gulp.watch([
-    'partials/*.html', 
-    'index.html',
-    '404.html',
-    'js/components/*.js',
-    'js/*.js',
-    'styles/css/*.css',
-    'img/logos/*.svg'
-], gulp.series('minify-css', 'bundle-js', 'minify-js', reload));
+gulp.task('build', gulp.series(clean, 'minify-css', 'minify-js'));
 
-gulp.task('browserSync', gulp.series('minify-css', 'bundle-js', 'minify-js', serve, watch));
+const watch = () => {
+    gulp.watch('styles/css/*.css', gulp.series('minify-css', stream));
+    gulp.watch(['js/components/*.js', 'js/*.js'], gulp.series('minify-js', reload));
+    gulp.watch(['partials/*.html', 'index.html', '404.html', 'img/logos/*.svg'], gulp.series(reload));
+}
+
+gulp.task('browserSync', gulp.series('build', serve, watch));

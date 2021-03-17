3ef02c526b06160d237fde13bc5c9e3a72568baa
charisTheo <dev@charistheo.io>
2021-03-17 21:40:41 +0000

    Added skip waiting for SW.

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
 self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
